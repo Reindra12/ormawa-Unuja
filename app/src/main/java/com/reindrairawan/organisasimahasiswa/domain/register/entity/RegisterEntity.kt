0a6850a732d926b87a7f953a5e1642ba80f2df74
reindra-ubuntu <reindrairawan@gmail.com>
2022-10-19 17:22:22 +0700

    register model

---
+++ app/src/main/java/com/reindrairawan/organisasimahasiswa/domain/register/entity/RegisterEntity.kt
@@ -0,0 +1,9 @@
+package com.reindrairawan.organisasimahasiswa.domain.register.entity
+
+data class RegisterEntity (
+    var id: Int,
+    var name: String,
+    var email: String,
+    var token: String
+
+)
