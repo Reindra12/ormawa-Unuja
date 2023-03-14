d59e84c1aa01d1c792f3a5e118693659e06a4fb6
Reindrairawan <reindrairawan@gmail.com>
2023-03-14 08:48:59 +0700

    repository kegiatanByIdJenisKegiatan

--- app/src/main/java/com/reindrairawan/organisasimahasiswa/domain/kegiatan/KegiatanRepository.kt
+++ app/src/main/java/com/reindrairawan/organisasimahasiswa/domain/kegiatan/KegiatanRepository.kt
@@ -1,4 +1,4 @@
-package com.reindrairawan.organisasimahasiswa.domain.searchview
+package com.reindrairawan.organisasimahasiswa.domain.kegiatan
 
 import com.reindrairawan.organisasimahasiswa.data.common.utils.WrappedListResponse
 import com.reindrairawan.organisasimahasiswa.data.common.utils.WrappedResponse
@@ -6,12 +6,15 @@ import com.reindrairawan.organisasimahasiswa.data.kegiatan.remote.dto.HistoryKeg
 import com.reindrairawan.organisasimahasiswa.data.kegiatan.remote.dto.HistoryPencarianResponse
 import com.reindrairawan.organisasimahasiswa.data.kegiatan.remote.dto.KegiatanResponse
 import com.reindrairawan.organisasimahasiswa.domain.common.base.BaseResult
-import com.reindrairawan.organisasimahasiswa.domain.searchview.entity.HistoryKegiatanEntity
-import com.reindrairawan.organisasimahasiswa.domain.searchview.entity.KegiatanEntity
+import com.reindrairawan.organisasimahasiswa.domain.kegiatan.entity.HistoryKegiatanEntity
+import com.reindrairawan.organisasimahasiswa.domain.kegiatan.entity.KegiatanEntity
 import kotlinx.coroutines.flow.Flow
 
 interface KegiatanRepository {
+
+    suspend fun kegiatanByIdJenis(id:Int) :Flow<BaseResult<List<KegiatanEntity>, WrappedListResponse<KegiatanResponse>>>
     suspend fun searchKegiatan(id: Int): Flow<BaseResult<List<HistoryKegiatanEntity>, WrappedListResponse<HistoryPencarianResponse>>>
     suspend fun getAllKegiatan(): Flow<BaseResult<List<KegiatanEntity>, WrappedListResponse<KegiatanResponse>>>
     suspend fun setHistoryKegiatan(historyKegiatanRequest: HistoryKegiatanRequest): Flow<BaseResult<HistoryKegiatanEntity, WrappedResponse<HistoryPencarianResponse>>>
+
 }
