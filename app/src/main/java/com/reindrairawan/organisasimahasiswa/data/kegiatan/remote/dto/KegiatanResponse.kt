97a4477b200c9e362eac17fdd6acf716c4c075e7
Reindrairawan <reindrairawan@gmail.com>
2023-03-14 08:45:54 +0700

    change respons kegiatan

--- app/src/main/java/com/reindrairawan/organisasimahasiswa/data/kegiatan/remote/dto/KegiatanResponse.kt
+++ app/src/main/java/com/reindrairawan/organisasimahasiswa/data/kegiatan/remote/dto/KegiatanResponse.kt
@@ -1,16 +1,16 @@
-package com.reindrairawan.organisasimahasiswa.data.searchview.remote.dto
+package com.reindrairawan.organisasimahasiswa.data.kegiatan.remote.dto
 
 import com.google.gson.annotations.SerializedName
 
 data class KegiatanResponse(
-    @SerializedName("id_kegiatan") var id_kegiatan:Int,
-    @SerializedName("nama_kegiatan") var nama_kegiatan:String,
-    @SerializedName("diskripsi_kegiatan") var diskripsi_kegiatan:String,
-    @SerializedName("gambar_kegiatan") var gambar_kegiatan:String,
-    @SerializedName("tgl_kegiatan") var tgl_kegiatan:String,
-    @SerializedName("jam_kegiatan") var jam_kegiatan:String,
-    @SerializedName("id_ormawa") var id_ormawa:Int,
+
+    @SerializedName("nama_kegiatan") var nama_kegiatan: String,
+    @SerializedName("diskripsi_kegiatan") var diskripsi_kegiatan: String,
+    @SerializedName("gambar_kegiatan") var gambar_kegiatan: String,
+    @SerializedName("tgl_kegiatan") var tgl_kegiatan: String,
+    @SerializedName("jam_kegiatan") var jam_kegiatan: String,
+    @SerializedName("id_ormawa") var id_ormawa: Int,
+    @SerializedName("id_jenis_kegiatan") var id_kegiatan: Int
 
 
-
-    )
+)
